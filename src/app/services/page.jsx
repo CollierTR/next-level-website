a7892a300d5e9f0fabcc18f9e00d218ccf6a3aa4
CollierTR <80648892+CollierTR@users.noreply.github.com>
2025-05-29 20:40:41 -0500

    Dynamically set the flip argument for the service cards

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -1,23 +1,23 @@
 import ContactCTA from "@/components/ContactCTA";
 import LargeServiceCard from "@/components/LargeServiceCard";
-import { projects } from "../../projects"
-import { services } from "../../services"
+import { projects } from "../../projects";
+import { services } from "../../services";
 
 export default function ProjectsPage() {
 	return (
 		<main className="text-2xl pt-40">
 			<h1 className="text-center text-7xl mb-20">Services</h1>
-            <div className="w-10/12 flex flex-col gap-20 justify-center place-items-center mx-auto">
-            {
-                services.map((service) => (
-                    <LargeServiceCard 
-                        key={service.name}
-                        title={service.name}
-                        description={service.description}
-                    />
-                ))
-            }
-            </div>
+			<div className="w-10/12 flex flex-col gap-20 justify-center place-items-center mx-auto">
+				{services.map((service, index) => (
+						<LargeServiceCard
+							key={service.name}
+							title={service.name}
+							description={service.description}
+                            flip={index%2===1}
+						/>
+					)
+				)}
+			</div>
 			<ContactCTA></ContactCTA>
 		</main>
 	);
