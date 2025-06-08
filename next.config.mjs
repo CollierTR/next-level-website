b73c14e1c2b3b66214ad2ebc715ce204aa9cf64a
CollierTR <80648892+CollierTR@users.noreply.github.com>
2025-06-07 23:35:45 -0500

    Refactored the MDX integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
+import createMDX from '@next/mdx'
+ 
 /** @type {import('next').NextConfig} */
 
-import withMDX from '@next/mdx';
 
-const withMDXConfig = withMDX({
-  extension: /\.mdx?$/,
-  options: {
-    // MDX plugins here if needed
-  },
-});
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-};
-
-export default withMDXConfig(nextConfig);
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+}
+ 
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+ 
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
